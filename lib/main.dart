687df19e379fe5a5bcd66efe8ba12fe6d7ad1730
atarxxia <84593153+ivryxx@users.noreply.github.com>
2023-03-13 01:41:19 +0900

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -10,82 +10,75 @@ class App extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: const Color(0xFF181818),
-        body:Padding(
+        body: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 40),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-            const SizedBox(
-              height: 80,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.
-              end,
-              children: [
-                Column(
-                  crossAxisAlignment: 
-                  CrossAxisAlignment.end,
-                  children: [
-                  const Text(
-                    'Hey, Selena',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 28,
-                    fontWeight: FontWeight.w800,
-                  ),
-                  ),
-                  Text(
-                    'Welcome back',
-                    style: TextStyle(
-                    color: Colors.white.
-                    withOpacity(0.8), fontSize: 18
-                  ),
-                  ),
+              const SizedBox(
+                height: 80,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      const Text(
+                        'Hey, Selena',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 28,
+                          fontWeight: FontWeight.w800,
+                        ),
+                      ),
+                      Text(
+                        'Welcome back',
+                        style: TextStyle(
+                            color: Colors.white.withOpacity(0.8), fontSize: 18),
+                      ),
+                    ],
+                  )
                 ],
-                )
-              ],
-            ),
-            const SizedBox(
-              height: 120,
-            ),
-            Text(
-              'Total Balance',
-              style: TextStyle(
-                fontSize: 22,
-                color: Colors.white.withOpacity(0.8),
               ),
-            ),
-            const SizedBox(
-              height: 5,
+              const SizedBox(
+                height: 120,
               ),
-            const Text(
-              '\$5 194 482',
-              style: TextStyle(
-                fontSize: 44,
-                fontWeight: FontWeight.w600,
-                color: Colors.white,
+              Text(
+                'Total Balance',
+                style: TextStyle(
+                  fontSize: 22,
+                  color: Colors.white.withOpacity(0.8),
+                ),
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              const Text(
+                '\$5 194 482',
+                style: TextStyle(
+                  fontSize: 44,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.white,
                 ),
               ),
               const SizedBox(
                 height: 30,
-                ),
+              ),
               Row(
                 children: [
                   Container(
                     decoration: BoxDecoration(
-                    color: const Color(0xFFF2B33A),
-                    borderRadius: BorderRadius.
-                    circular(45)
-                    ),
+                        color: const Color(0xFFF2B33A),
+                        borderRadius: BorderRadius.circular(45)),
                     child: const Padding(
-                      padding:EdgeInsets.symmetric(
+                      padding: EdgeInsets.symmetric(
                         vertical: 20,
                         horizontal: 50,
                       ),
-                      child: Text('Transfer',
-                        style: TextStyle(
-                          fontSize: 20
-                        ),
+                      child: Text(
+                        'Transfer',
+                        style: TextStyle(fontSize: 20),
                       ),
                     ),
                   )
